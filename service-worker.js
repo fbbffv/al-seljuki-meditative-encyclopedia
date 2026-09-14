9238a756bdc7fe49bf6f18ccb7c8af64bd557df6
fbbffv <tahirnajim1987@gmail.com>
2026-09-15 00:00:43 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
   - كل مقال يزوره المستخدم مرة واحدة (بإنترنت) يُصبح متاحاً دائماً بعدها بلا إنترنت.
 */
 
-const CACHE_VERSION = 'suljuki-v4';
+const CACHE_VERSION = 'suljuki-v8';
 const CORE_ASSETS = [
   './',
   './index.html',
